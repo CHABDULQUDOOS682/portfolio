7507edc018a52b9870322686f8e1072017b3659d
CHABDULQUDOOS682 <abdulqudoos682@gmail.com>
2025-03-16 02:08:16 +0500

    Add Dashboard

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".toggle-btn").forEach(button => {
+        button.addEventListener("click", function () {
+            let content = this.parentElement.nextElementSibling;
+            if (content.style.display === "none") {
+                content.style.display = "block";
+                this.textContent = "x";
+            } else {
+                content.style.display = "none";
+                this.textContent = "+";
+            }
+        });
+    });
+});
